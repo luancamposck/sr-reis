3c9f2c934caa1025d2eae6be6d4d0c6dd8c3f567
luancamposck <luancamposck@gmail.com>
2026-05-11 21:52:20 -0300

    style(web): format home page placeholder

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,10 @@
 const Home = () => {
-    return (
-        <main>
-            <h1>Hello World</h1>
-            <div>Sr. Reis</div>
-        </main>
-    );
-};
+	return (
+		<main>
+			<h1>Hello World</h1>
+			<div>Sr. Reis</div>
+		</main>
+	)
+}
 
-export default Home;
+export default Home
